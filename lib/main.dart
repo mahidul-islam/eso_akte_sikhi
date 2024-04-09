c3b44c995c9d043695310f4e7e8462a17bac85f7
mahidul-islam <mizihan84@gmail.com>
2024-04-09 17:53:39 +0600

    running on macos

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,13 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
     GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
