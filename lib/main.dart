5acb832a363b1f358b8debd2f543f1111e564d36
mahidul-islam <mizihan84@gmail.com>
2024-04-09 17:26:05 +0600

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:get/get.dart';
-
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     GetMaterialApp(
       title: "Application",
