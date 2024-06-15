33495e9e930ffbc00987b152e0d3847b88232d6b
mzahin01 <minhazahin@gmail.com>
2024-06-15 15:44:43 +0600

    made good

--- lib/app/modules/editor_page/views/editor_page_view.dart
+++ lib/app/modules/editor_page/views/editor_page_view.dart
@@ -18,47 +18,62 @@ class EditorPageView extends GetView<EditorPageController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(100.0),
+        preferredSize: const Size.fromHeight(80.0),
         child: Container(
+          margin: const EdgeInsets.fromLTRB(10, 60, 10, 0),
           decoration: const BoxDecoration(
-            color: Colors.white,
+            color: Colors.purpleAccent,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25)),
             border: Border(
-              bottom: BorderSide(color: Colors.orange, width: 10.0),
+              bottom: BorderSide(color: Colors.purpleAccent, width: 10.0),
+              right: BorderSide(color: Colors.purpleAccent, width: 10.0),
+              left: BorderSide(color: Colors.purpleAccent, width: 10.0),
             ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        Get.toNamed(Routes.HOME);
-                      },
-                      child: Image.asset(
-                        'assets/backButton.png',
-                        width: 40,
-                        height: 40,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25)),
+              border: Border(
+                bottom: BorderSide(color: Colors.orange, width: 10.0),
+                right: BorderSide(color: Colors.orange, width: 10.0),
+                left: BorderSide(color: Colors.orange, width: 10.0),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Get.toNamed(Routes.HOME);
+                        },
+                        child: Image.asset(
+                          'assets/backButton.png',
+                          width: 40,
+                          height: 40,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: Get.width / 1.2,
-                    child: const Center(
+                    const Expanded(
+                      child: Center(
                         child: Tex(
                             rcvd: 'SELECT DRAWING',
                             fsize: 25,
-                            colo: Colors.red)),
-                  )
-                ],
-              ),
-              Container(
-                height: 10,
-                color: Colors.purple,
-              ),
-            ],
+                            colo: Colors.red),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
